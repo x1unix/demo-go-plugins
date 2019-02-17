e3f3d743ff405e9ee60a5b438fc4bb0bae6a59b5
x1unix <denis0051@gmail.com>
2019-02-18 01:59:29 +0200

    Add config loader

--- server/config/config.go
+++ server/config/config.go
@@ -1,9 +1,42 @@
 package config
 
-import "github.com/x1unix/demo-go-plugins/server/feed/sources"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/x1unix/demo-go-plugins/server/feed/sources"
+	"io/ioutil"
+)
+
+// Main is main configuration
+var Main Config
 
 type Config struct {
 	Listen  string      `json:"listen"`
 	Debug   bool        `json:"debug"`
 	Sources sources.Set `json:"sources"`
+}
+
+// Load loads configuration from file
+func Load(configPath string) (err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("failed to read config from file '%s': %s", configPath, err)
+		}
+	}()
+
+	raw, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return err
+	}
+
+	if !json.Valid(raw) {
+		return errors.New("found JSON syntax error")
+	}
+
+	if err := json.Unmarshal(raw, &Main); err != nil {
+		return fmt.Errorf("failed to parse JSON file (%s)", err)
+	}
+
+	return err
 }
