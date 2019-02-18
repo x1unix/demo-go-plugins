d7ed34a0857c4f207f202c3a1ba69d14665c293b
x1unix <denis0051@gmail.com>
2019-02-18 07:53:30 +0200

    reddit: fix image defaults

--- sources/reddit/posts.go
+++ sources/reddit/posts.go
@@ -5,8 +5,6 @@ import (
 	"github.com/x1unix/demo-go-plugins/server/feed"
 )
 
-const noImage = "self"
-
 func submissionsToPosts(subs []*geddit.Submission) feed.Posts {
 	result := make(feed.Posts, 0, len(subs))
 	for _, s := range subs {
@@ -15,12 +13,11 @@ func submissionsToPosts(subs []*geddit.Submission) feed.Posts {
 			URL:        s.URL,
 			Title:      s.Title,
 			Text:       s.SelftextHTML,
-			ImageURL:   s.ThumbnailURL,
 			SourceType: name,
 			CreatedAt:  s.DateCreated,
 		}
 
-		if s.ThumbnailURL != noImage {
+		if s.ThumbnailURL != "self" && s.ThumbnailURL != "default" {
 			post.ImageURL = s.ThumbnailURL
 		}
 
