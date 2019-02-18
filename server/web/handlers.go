3bb787d780960216bf2b296c277330dc1603b0db
x1unix <denis0051@gmail.com>
2019-02-18 02:46:22 +0200

    Add sources list endpoint

--- server/web/handlers.go
+++ server/web/handlers.go
@@ -1,7 +1,15 @@
 package web
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"github.com/x1unix/demo-go-plugins/server/feed"
+	"net/http"
+)
 
 func registerHandlers(r *mux.Router) {
-
+	r.Path("/sources").Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		OK(response{
+			"sources": feed.SourceNames(),
+		}, w)
+	})
 }
