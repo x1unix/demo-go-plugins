37365c8434d17063bc34ef6725766eaecbec8498
x1unix <denis0051@gmail.com>
2019-02-18 04:52:28 +0200

    twitter: image workaround

--- sources/reddit/posts.go
+++ sources/reddit/posts.go
@@ -5,10 +5,12 @@ import (
 	"github.com/x1unix/demo-go-plugins/server/feed"
 )
 
+const noImage = "self"
+
 func submissionsToPosts(subs []*geddit.Submission) feed.Posts {
 	result := make(feed.Posts, 0, len(subs))
 	for _, s := range subs {
-		result = append(result, feed.Post{
+		post := feed.Post{
 			ID:         s.ID,
 			URL:        s.URL,
 			Title:      s.Title,
@@ -16,7 +18,13 @@ func submissionsToPosts(subs []*geddit.Submission) feed.Posts {
 			ImageURL:   s.ThumbnailURL,
 			SourceType: name,
 			CreatedAt:  s.DateCreated,
-		})
+		}
+
+		if s.ThumbnailURL != noImage {
+			post.ImageURL = s.ThumbnailURL
+		}
+
+		result = append(result, post)
 	}
 
 	return result
